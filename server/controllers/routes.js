d0b0d2eafd016ec72d1749395a6c9af7825fcd20
Reid Henderson <reid.henderson@gmail.com>
2017-07-15 11:23:01 -0400

    added path module to routes.js

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 module.exports = function routes (app, passport) {
   // TODO: figure out cleaner import/export method
@@ -7,7 +8,7 @@ module.exports = function routes (app, passport) {
   require('./routes/bandRoutes.js')(app);
   require('./routes/eventRoutes.js')(app);
   require('./routes/reviewRoutes.js')(app);
-  require('./routes/testRoutes.js')(app);
+  require('./routes/reviewRoutes.js')(app);
 
   //Catch-all directs everything else to react front end/index.html
   app.get('*', (req, res) => {
