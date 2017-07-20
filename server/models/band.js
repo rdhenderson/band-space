7789fb4bf8cbe27f39c06bb1ca8ea86b8898a5e4
Reid Henderson <reid.henderson@gmail.com>
2017-07-20 05:54:23 -0400

    connected createGroup form to database.

--- server/models/band.js
+++ server/models/band.js
@@ -1,22 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const findOrCreate = require('mongoose-find-or-create');
 
 const BandSchema = new Schema({
   name: { type: String, trim: true, required: true },
-  profile: { 
-    type: Schema.Types.ObjectId,
-    trim: true,
-    ref: 'Profile',
+  address: {
+    street: { type: String, trim: true },
+    city: { type: String, trim: true, defaultValue: "Washington"},
+    state: { type: String, trim: true, defaultValue: "DC" },
   },
-  spotify: {                   },
+  phone: { type: String, trim: true },
+  members: [{
+    user_id: {type: Schema.Types.ObjectId, ref: 'User'},
+    name: { type: String, trim: true },
+    email: { type: String, trim: true },
+    instrument: { type: String, trim: true },
+  }],
+  images: [{ type: String, trim: true }],
+  spotify: { type: String, trim: true },
   artist_id: [{
      type: Schema.Types.ObjectId,
       ref: 'Artist',
   }],
+  profile: {
+    type: Schema.Types.ObjectId,
+    trim: true,
+    ref: 'Profile',
+  },
 
   created_date: { type: Date, default: Date.now },
   updated_date: { type: Date, default: Date.now },
 });
+
+BandSchema.plugin(findOrCreate);
 
 const Band = mongoose.model("Band", BandSchema);
 
