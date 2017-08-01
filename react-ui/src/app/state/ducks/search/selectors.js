1501a61558a8cd3ce45a686ad4f05ec70b02491a
Reid Henderson <reid.henderson@gmail.com>
2017-08-01 14:57:18 -0400

    Updated displayList logic and connections.

--- react-ui/src/app/state/ducks/search/selectors.js
+++ react-ui/src/app/state/ducks/search/selectors.js
@@ -3,9 +3,9 @@ import { searchType } from './types'
 
 const getSearchQuery = state => state.search.query;
 const getSearchType = state => state.search.searchType;
-const getGroupList = state => state.group.groupList || [];
-const getUserList = state => state.user.userList || [];
-const getVenueList = state => state.venue.venueList || [];
+const getGroupList = state => state.group.groupList;
+const getUserList = state => state.user.userList;
+const getVenueList = state => state.venue.venueList;
 
 function getNameIdTypeList(list, listType){
   if (!list || list.length < 1) return [];
@@ -21,29 +21,22 @@ function getFullTypeList(list, listType){
 export const getLimitedDisplayList = createSelector(
   [getUserList, getGroupList, getVenueList, getSearchType, getSearchQuery],
   (users, groups, venues, type, query) => {
-    console.log("venue list", venues);
-    const lists = { users, groups, venues, all: [...users, ...venues, ...groups]};
-    const list = lists[type];
-    return list;
-    // if (list && list.length > 0) return list;
-    // return [];
-    // const filteredList = list.filter((item) => item.name.toUpperCase().indexOf(query.toUpperCase()) > -1)
-    //
-    // if (!query || query.length < 1) return getNameIdTypeList(list);
-    // return getNameIdTypeList(filteredList);
+    [getUserList, getGroupList, getVenueList, getSearchType, getSearchQuery],
+    (users, groups, venues, type, query) => {
+      const lists = { users, venues, groups };
+      const list = lists[type];
+      if (!list || list.length < 1) return [];
+      if (!query || query.length < 1) return getNameIdTypeList(list);
+      return getNameIdTypeList(list.filter((item) => item.name.toUpperCase().indexOf(query.toUpperCase()) > -1));
   });
 
 // Returns full object for each matching element with list field showing element type
 export const getFullDisplayList = createSelector(
   [getUserList, getGroupList, getVenueList, getSearchType, getSearchQuery],
   (users, groups, venues, type, query) => {
-    const lists = { users, venues, groups, all: [...users, ...venues, ...groups]};
+    const lists = { users, venues, groups };
     const list = lists[type];
-    return list;
-    console.log("venue list", venues);
-    console.log("list object venues list", lists['venues']);
-
     if (!list || list.length < 1) return [];
-    if (!query || query.length < 1) return getNameIdTypeList(list);
-    return getNameIdTypeList(list.filter((item) => item.name.toUpperCase().indexOf(query.toUpperCase()) > -1));
+    if (!query || query.length < 1) return getFullTypeList(list);
+    return getFullTypeList(list.filter((item) => item.name.toUpperCase().indexOf(query.toUpperCase()) > -1));
   });
